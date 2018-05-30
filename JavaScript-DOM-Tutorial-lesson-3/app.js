0d6609e9dd57eccd4460f8d2d975664b954c691b
Urbenson Laurent <ulaurent12@gmail.com>
2018-05-30 03:00:28 +0000

    Added files for DOM tutorial

---
+++ JavaScript-DOM-Tutorial-lesson-3/app.js
@@ -0,0 +1,23 @@
+// This program shows multiple ways of querying the DOM
+
+// Getting an elemnet through querySelector
+// #book-list is parent node
+// li is the child node of book-list with :nth-child(2) being the second on the list
+// .name is the class name of the second node in the list
+
+const wmf = document.querySelector('#book-list li:nth-child(2) .name');
+//console.log(wmf);
+
+//
+var books = document.querySelector('#book-list li .name');
+//console.log(books);
+
+books = document.querySelectorAll('#book-list li .name');
+console.log(books);
+
+// Array.from(books) changes the books span collection into an array
+// the .forEach statement allows you to go through each element within the array
+
+Array.from(books).forEach(function(book){
+    console.log(book);
+})
